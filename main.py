54c3abf72dce4776ea6fc487d49312450693c66a
Charlie <charlieedoherty@gmail.com>
2024-04-12 20:41:54 -0500

    Update main to connect the input handler file

--- main.py
+++ main.py
@@ -5,3 +5,17 @@ HackKU - University of Kansas
 
 Run the main function in this file to begin the program
 """
+from inputhandler import input_handler
+
+if __name__ == "__main__":
+
+    print("Hello, welcome to our base conversion project for HackKU 2024")
+    # Use input to force an input to continue
+    input("Press 'enter' to continue...\n")
+
+    in_base, out_base, in_number = input_handler()
+
+    # If the base is the same, return the same number
+    if in_base == out_base:
+        print(f"Base {in_base}: {in_number}\n"
+              f"Base {out_base}: {in_number}")
