335f14acb8dae5ae8493065d7e48e6a2c63e573c
Charlie <charlieedoherty@gmail.com>
2024-04-12 23:07:11 -0500

    Exclude base 1 in output base

--- inputhandler.py
+++ inputhandler.py
@@ -22,7 +22,7 @@ def input_handler():
         # Check for invalid input
         try:
             out_base = int(input("What output base would you like to use: "))
-            if 10 > out_base > 0:
+            if 10 > out_base > 1:
                 break
             else:
                 raise RuntimeError
