51c31d7a8416553901f9d42ae43d43f7ee32deba
JonathanGott <143655464+JonathanGott@users.noreply.github.com>
2024-04-12 20:47:30 -0500

    test

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from inputhandler import input_handler
 
 if __name__ == "__main__":
 
-    print("Hello, welcome to our base conversion project for HackKU 2024")
+    print("Hello, welcome to our base conversion project for HackKU 2024!")
     # Use input to force an input to continue
     input("Press 'enter' to continue...\n")
 
