f64edc5a322789e08fdc932d27dc44940a8d4093
Charlie <charlieedoherty@gmail.com>
2024-04-12 23:06:09 -0500

    Fix formatting and exclude base 1

--- inputhandler.py
+++ inputhandler.py
@@ -5,30 +5,30 @@ def input_handler():
         # Check for invalid input
         try:
             in_base = int(input("What input base would you like to use: "))
-            if 10 > in_base > 0:
+            if 10 >= in_base > 1:
                 break
             else:
                 raise RuntimeError
 
         except RuntimeError:
-            print("base system bust be between 1 and 10")
+            print("Base system must be between 1 and 10")
 
         except ValueError:
-            print("That isn't a valid input. Try an integer (2).")
+            print("That isn't a valid input. Try a natural number (Example: 5)")
 
     # Get user input for the output base
     while True:
 
         # Check for invalid input
         try:
-            out_base = int(input("What input base would you like to use: "))
+            out_base = int(input("What output base would you like to use: "))
             if 10 > out_base > 0:
                 break
             else:
                 raise RuntimeError
 
         except RuntimeError:
-            print("base system bust be between 1 and 10")
+            print("Base system bust be between 1 and 10")
 
         except ValueError:
             print("That isn't a valid input. Try an integer (2).")
@@ -38,12 +38,13 @@ def input_handler():
 
         # Check for invalid input
         try:
-            in_number = input(f"What number would you like to translate in base {in_base}: ")
+            in_number = input(f"What number would you like to translate from base {in_base} to base {out_base}: ")
 
             # Check every digit for a number greater than the base system can handle
             for digit in in_number:
                 if int(digit) >= in_base:
                     raise RuntimeError
+
             # If the loop never catches an invalid number then continue
             break
 
